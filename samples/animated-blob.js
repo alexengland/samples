5f37d5f73ddc60c58ad5a70a2e0ac4969fb2b909
Alex England <alex.england@icloud.com>
2023-10-13 14:53:27 +1100

    Update Examples

--- samples/animated-blob.js
+++ samples/animated-blob.js
@@ -1,57 +1,57 @@
-    mpa.animateBlob = function() {
-        'use strict';
-        var blob1, blob2, blob3, current, i, radius = [], random;
+mpa.animateBlob = function() {
+    'use strict';
+    var blob1, blob2, blob3, current, i, radius = [], random;
 
-        blob1 = document.getElementById('blob1');
-        blob2 = document.getElementById('blob2');
-        blob3 = document.getElementById('blob3');
+    blob1 = document.getElementById('blob1');
+    blob2 = document.getElementById('blob2');
+    blob3 = document.getElementById('blob3');
 
-        function randomPercentage() {
-            return Math.round(Math.random() * (100 - 1) + 1);
-        }
+    function randomPercentage() {
+        return Math.round(Math.random() * (100 - 1) + 1);
+    }
 
-        function randomValues() {
+    function randomValues() {
 
-            radius = []
+        radius = []
 
-            for (i = 0; i < 9; i++) {
+        for (i = 0; i < 9; i++) {
 
-                if (i === 4) {
+            if (i === 4) {
 
-                    radius.push('/');
+                radius.push('/');
 
-                } else {
+            } else {
 
-                    radius.push(randomPercentage() + '%');
-
-                }
+                radius.push(randomPercentage() + '%');
 
             }
-            return radius.join(' ');
 
         }
+        return radius.join(' ');
 
-        if (blob1) {
-
-            blob1.style.borderRadius = randomValues();
-            blob2.style.borderRadius = randomValues();
-            blob3.style.borderRadius = randomValues();
-
-        }
-
-    };
-
-    @keyframes lava {
-
-        0%     { filter:brightness(90%); }
-        50.0%  { filter:brightness(120%); }
-        100.0% { filter:brightness(90%); }
     }
 
-    .blob {
-        filter:brightness(90%);
-        -webkit-animation: lava 7000ms infinite ease-in-out;
-        animation: lava 7000ms infinite ease-in-out;
-        -webkit-transition:all 1500ms linear;
-        transition:all 1500ms linear;
+    if (blob1) {
+
+        blob1.style.borderRadius = randomValues();
+        blob2.style.borderRadius = randomValues();
+        blob3.style.borderRadius = randomValues();
+
     }
+
+};
+
+@keyframes lava {
+
+    0%     { filter:brightness(90%); }
+    50.0%  { filter:brightness(120%); }
+    100.0% { filter:brightness(90%); }
+}
+
+.blob {
+    filter:brightness(90%);
+    -webkit-animation: lava 7000ms infinite ease-in-out;
+    animation: lava 7000ms infinite ease-in-out;
+    -webkit-transition:all 1500ms linear;
+    transition:all 1500ms linear;
+}
